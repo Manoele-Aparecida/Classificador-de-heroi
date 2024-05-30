2208b2b5c570d2ad82f9e65afb73fdf495d5ea65
Manoele-Aparecida <manoeleparecida14@gmail.com>
2024-05-30 20:48:00 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//classificador de heroi
+console.log("Digita o nome do seu jogador")
+
+//declara uma variável
+let nickname = "Edlipe"
+
+console.log("Bem vinda " + nickname)
+
+let nivelDoEdlipe = 1
+if(nivelDoEdlipe === 1){
+    console.log("9.001")
+    }else if(nivelDoEdlipe ===2){
+    console.log("10.000")
+    }
